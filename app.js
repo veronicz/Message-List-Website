aabff94dcd844be2109a4799f58536edba5877c2
sz <v.sherryzzz@gmail.com>
2019-07-09 19:40:09 -0700

    make server ready for heroku

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -49,6 +50,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 module.exports = app;
